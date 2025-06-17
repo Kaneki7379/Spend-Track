83df054c1fdb645e82de373db61c15afd9f5f9a4
kaneki7379 <singh.yash9919@gmail.com>
2025-06-18 01:57:29 +0530

    all dependency included

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,31 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/header";
-const inter = Inter({subsets:["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 import { ClerkProvider } from "@clerk/nextjs";
+
 export const metadata = {
   title: "Spend Track",
-  description: "One Stop Finance Platfrom",
+  description: "One Stop Finance Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        <Header/>
-        <main className="min-h-screen"> 
-        {children}
-        </main>
-        {/* Footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>&copy; 2025</p>
-          </div>
-        </footer>
+      <body className={`${inter.className}`}>
+        <ClerkProvider>
+          <Header />
+          <main className="min-h-screen">
+            {children}
+          </main>
+          {/* Footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>&copy; 2025</p>
+            </div>
+          </footer>
+        </ClerkProvider>
       </body>
     </html>
-  </ClerkProvider>
   );
 }
